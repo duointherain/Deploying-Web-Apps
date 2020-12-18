7851729feaa6bafbb30ba871e64549c986f9383c
duointherain <duointherain@gmail.com>
2020-12-17 20:50:25 -0800

    starting

---
+++ max-port/src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1>Here comes my portfolio</h1>
+       <h2>It's going to be better this time.</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
